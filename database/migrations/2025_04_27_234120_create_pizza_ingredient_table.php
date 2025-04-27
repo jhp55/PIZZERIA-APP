13b2fcaf48d4ec5f7d40bfdf0307a35bf676cab9
VictorMarin8 <victormarin3890@gmail.com>
2025-04-27 18:48:39 -0500

    Migraciones de ingredients,pizza_ingredient,extra_ingredients,branches y orders

---
+++ database/migrations/2025_04_27_234120_create_pizza_ingredient_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('pizza_ingredient', function (Blueprint $table) {
+            $table->foreignId('pizza_id')->constrained('pizzas')->onDelete('cascade');
+            $table->foreignId('ingredient_id')->constrained('ingredients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pizza_ingredient');
+    }
+};

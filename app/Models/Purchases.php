6620cf870b4202b69abf24dd5c997a205bf5f97e
Juand7M <trabajosfcecep4@gmail.com>
2025-05-04 04:44:05 -0500

    pucharses model

---
+++ app/Models/Purchases.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchases extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'supplier_id',
+        'raw_material_id',
+        'quantity',
+        'purchase_price',
+    ];
+
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    /**
+     * Relación con la materia prima
+     */
+    public function rawMaterial()
+    {
+        return $this->belongsTo(RawMaterial::class);
+    }
+}
